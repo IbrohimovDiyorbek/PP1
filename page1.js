733476b0d1df7301b701f8dec380a669599d1add
IbrohimovDiyorbek <diyorbekibrohimov31@gmail.com>
2024-07-07 22:20:31 +0500

    it is a new project

---
+++ page1.js
@@ -0,0 +1,23 @@
+document.getElementById('showFormBtn').addEventListener('click', function() {
+    document.getElementById('formContainer').style.display = 'block';
+    this.style.display = 'none';
+});
+
+document.getElementById('userForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const name = document.getElementById('name').value;
+    const age = document.getElementById('age').value;
+    const phone = document.getElementById('phone').value;
+    const gender = document.getElementById('gender').value;
+
+    const userInfo = `
+        <p><strong>Name:</strong> ${name}</p>
+        <p><strong>Age:</strong> ${age}</p>
+        <p><strong>Ring number:</strong> ${phone}</p>
+        <p><strong>Gender:</strong> ${gender}</p>
+    `;
+
+    document.getElementById('userInfo').innerHTML = userInfo;
+    document.getElementById('userForm').reset();
+});
