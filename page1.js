8b61a15619f9b11010de44aac6956b27e694298f
IbrohimovDiyorbek <diyorbekibrohimov31@gmail.com>
2024-07-09 21:53:30 +0500

    it is a new page

--- page1.js
+++ page1.js
@@ -5,17 +5,28 @@ document.getElementById('showFormBtn').addEventListener('click', function() {
 
 document.getElementById('userForm').addEventListener('submit', function(event) {
     event.preventDefault();
-    
+
+    const avary = document.getElementById('avary').value;
+    const line = document.getElementById('line').value;
     const name = document.getElementById('name').value;
     const age = document.getElementById('age').value;
     const phone = document.getElementById('phone').value;
-    const gender = document.getElementById('gender').value;
+    const size = document.getElementById('size').value;
+    const contry = document.getElementById('contry').value;
+    const cer = document.getElementById('cer').value;
+    const info = document.getElementById('info').value;
 
     const userInfo = `
-        <p><strong>Name:</strong> ${name}</p>
-        <p><strong>Age:</strong> ${age}</p>
+        <p><strong>Bird Aviary:</strong> ${avary}</p>
+        <p><strong>Bird name:</strong> ${name}</p>
+        <p><strong>Bird Line:</strong> ${line}</p>
         <p><strong>Ring ID:</strong> ${phone}</p>
+        <p><strong>Age:</strong> ${age}</p>
+        <p><strong>Ring Size:</strong> ${size}</p>
+        <p><strong>Contry:</strong> ${contry}</p>
+        <p><strong>Certificate:</strong> ${cer}</p>
         <p><strong>Gender:</strong> ${gender}</p>
+        <p><strong>Information:</strong> ${info}</p>
     `;
 
     document.getElementById('userInfo').innerHTML = userInfo;
